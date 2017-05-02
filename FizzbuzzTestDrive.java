ab3f8bab35e1d74c6b48475b76daf0a5c8a02210
Tom Fuller <tomf95@hotmail.co.uk>
2017-05-02 09:33:54 +0100

    files set up

---
+++ FizzbuzzTestDrive.java
@@ -0,0 +1,14 @@
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.hamcrest.core.CombinableMatcher;
+import org.junit.Test;
+
+class FizzbuzzTestDrive {
+
+  public static void main(String[] args) {
+    FizzbuzzTestDrive tests = new FizzbuzzTestDrive();
+    Fizzbuzz fizz = new Fizzbuzz();
+  }
+
+}
